7eb08060e4c30077cd9b5cc58a522ed8beefc10f
Christian Gorenflo <cgorenflo@uwaterloo.ca>
2016-12-21 02:36:50 -0500

    fixed imports

--- iss4e/webike/trips/__init__.py
+++ iss4e/webike/trips/__init__.py
@@ -1,6 +1,7 @@
-from iss4e.webike.trips.trip import *
-from iss4e.webike.trips.trip_handler import *
-from iss4e.webike.trips.event import *
-from iss4e.webike.trips.trip_detector import *
-from iss4e.webike.trips.rule import *
 from iss4e.webike.trips.imei import *
+from iss4e.webike.trips.event import *
+from iss4e.webike.trips.rule import *
+from iss4e.webike.trips.trip import *
+from iss4e.webike.trips.trip_detector import *
+from iss4e.webike.trips.trip_handler import *
+
