0383471f6c68b9a4969f9209e2f42f07cb9a6f3f
Christian Gorenflo <cgorenflo@uwaterloo.ca>
2016-12-21 01:29:37 -0500

    add setup

--- iss4e/webike/trips/manual_execution_script.py
+++ iss4e/webike/trips/manual_execution_script.py
@@ -1,15 +1,11 @@
-from  itertools import chain
+from itertools import chain
 from iss4e.db import influxdb
-from iss4e.util import BraceMessage as __, logging
 from iss4e.util.config import load_config
 
 from iss4e.webike.trips import module_locator, TripDetector
 from iss4e.webike.trips.imei import IMEI
 
 config = load_config(module_locator.module_path())
-logger = logging.getLogger("iss4e.webike.db")
-
-
 
 with influxdb.connect(**config["webike.influx"]) as client:
     measurement = client.stream_measurement(measurement="sensor_data",
---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='webike-trip_detection',
+    version='0.5.0',
+    url='https://github.com/iss4e/webike-trip-detection',
+    license='MIT',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.5',
+    ],
+    author='Information Systems and Science for Energy',
+    author_email='webike-dev@lists.uwaterloo.ca',
+    description='WeBike trip detection',
+    packages=find_packages(),
+    install_requires=[
+        'iss4e_toolchain>=0.1.0'
+    ]
+)
