d1a9a77d420cfae0e1632da1af17e3c0e126e04e
Christian Gorenflo <cgorenflo@uwaterloo.ca>
2016-12-21 01:50:32 -0500

    add concurrency

--- iss4e/webike/trips/manual_execution_script.py
+++ iss4e/webike/trips/manual_execution_script.py
@@ -1,3 +1,4 @@
+from concurrent.futures import ProcessPoolExecutor, wait
 from itertools import chain
 from iss4e.db import influxdb
 from iss4e.util.config import load_config
@@ -5,13 +6,20 @@ from iss4e.util.config import load_config
 from iss4e.webike.trips import module_locator, TripDetector
 from iss4e.webike.trips.imei import IMEI
 
-config = load_config(module_locator.module_path())
 
+def _execute(series, samples):
+    trips = TripDetector(IMEI(series)).processSamples(samples)
+    points = chain.from_iterable((trip.to_points() for trip in trips))
+    client.write_points(points)
+
+
+config = load_config(module_locator.module_path())
 with influxdb.connect(**config["webike.influx"]) as client:
     measurement = client.stream_measurement(measurement="sensor_data",
                                             fields=["time", "discharge_current", "linear_acceleration_x",
                                                     "linear_acceleration_y", "linear_acceleration_z"])
-    for _, series, samples in measurement:
-        trips = TripDetector(IMEI(series)).processSamples(samples)
-        points = chain.from_iterable((trip.to_points() for trip in trips))
-        client.write_points(points)
+
+    with ProcessPoolExecutor(max_workers=14) as executor:
+        futures = [executor.submit(_execute, series, samples) for _, series, samples in measurement]
+
+        wait(futures)
