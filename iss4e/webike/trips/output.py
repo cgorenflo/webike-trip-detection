7424406b8f5efb1d872ebb773e5e770938c0062a
Christian Gorenflo <cgorenflo@uwaterloo.ca>
2017-01-01 21:20:14 -0500

    additional logging

--- iss4e/webike/trips/output.py
+++ iss4e/webike/trips/output.py
@@ -1,6 +1,9 @@
+import logging
+
 from iss4e.webike.trips.event import Event
 from iss4e.webike.trips.sample import Sample
 from iss4e.webike.trips.trip import Trip
+
 
 
 class InfluxPoints(object):
@@ -38,12 +41,19 @@ class MySqlInsertQuery(object):
     def _collect(self, trip: Trip):
         if not trip.content:
             return
+
+        logger = logging.getLogger("webike.trips")
+        logger.debug({"imei": trip.content[0].imei, "start": trip.content[0]["time"], "end": trip.content[-1]["time"]})
         self._values.append(
             {"imei": trip.content[0].imei, "start": trip.content[0]["time"], "end": trip.content[-1]["time"]})
 
     def to_string(self):
-        return "INSERT INTO {table} {columns} VALUES {values};".format(table="trips", columns="(IMEI,start,end)",
-                                                                       values=self._get_values())
+        values = self._get_values()
+        if values:
+            return "INSERT INTO {table} {columns} VALUES {values};".format(table="trips", columns="(IMEI,start,end)",
+                                                                       values=values)
+        else:
+            return ""
 
     def _get_values(self):
         ",".join("({imei},{start},{end})".format(**value) for value in self._values)
